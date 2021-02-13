3d37ead2115c24a4ea8b474f21d71fdfadf8c69a
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-02-12 20:17:09 -0800

    IT WORRRKSSSSSS

--- src/components/PokeItem.js
+++ src/components/PokeItem.js
@@ -6,9 +6,9 @@ class PokeItem extends React.Component {
       <li className="poke-item">
         <h2 className="poke-name">{this.props.PokeItemProp.pokemon}</h2>
         <img className="poke-img" src={this.props.PokeItemProp.url_image} alt="animals"/>
-        <p className="poke-type">{this.props.PokeItemProp.ability_1}</p>
-        <p className="poke-type">{this.props.PokeItemProp.type_1}</p>
-        <p className="poke-type">{this.props.PokeItemProp.shape.id}</p>
+        <p className="poke-type">Ability: {this.props.PokeItemProp.ability_1}</p>
+        <p className="poke-type">Type: {this.props.PokeItemProp.type_1}</p>
+        <p className="poke-shape">Shape: {this.props.PokeItemProp.shape}</p>
       </li>
     )
   }
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -6,40 +6,75 @@ export default class SearchPage extends React.Component {
     // initalize state //
     state = {
       pokemon: pokeData,
-      sortBy: '',
-      sortOrder:'',
+      sortBy: 'pokemon',
+      sortOrder:'Ascending',
       filter:'',
     }
+
+
+    // changing state for all our options to sort/filter the data //
   
-    handleSearchChange = (e) => {
+    handleSearch = (e) => {
+        // this is invoked when the user searches for a poke //
         this.setState({
            filter: e.target.value, 
         });
     }
 
+    handleSortBy = (e) => {
+        // this invoked when the user searches for a poke based off category //
+        this.setState({
+           sortBy: e.target.value, 
+        });
+    }
+
+    handleSortOrder = (e) => {
+        // this invoked when the user sorts order asc/decs//
+        this.setState({
+           sortOrder: e.target.value, 
+        });
+    }
+
+    
+
    render () {
-            
-    const filteredPokeData = pokeData.filter(pokemon=> pokemon.pokemon.includes(this.state.filter))
+
+    if (this.state.sortOrder === 'Ascending') {
+        pokeData.sort(
+            (a, b) =>
+                a[this.state.sortBy].localeCompare(b[this.state.sortBy])
+        );
+    } else {
+        pokeData.sort(
+            (a, b) =>
+                b[this.state.sortBy].localeCompare(a[this.state.sortBy])
+        );
+    }
+    
+    
+    const filteredPokeData = this.state.pokemon.filter(pokemon=> pokemon.pokemon.includes(this.state.filter))
 
     return (
     
         <div className="search">
-            <div className="search-bar">
-                <input type="text" onChange={this.handleSearchChange}></input>
-            </div>
             <div className='dropdown'>
-                <select className="poke-sort">
+                Sort By Category:
+                <select className="poke-sort" onChange={this.handleSortBy}>
                     <option value="pokemon">Name</option>
                     <option value="ability_1">Ability</option>
                     <option value="type_1">Type</option>
-                    <option value="shape_id">Shape</option>
+                    <option value="shape">Shape</option>
                 </select>
             </div>
             <div className='dropdown'>
-                <select className="poke-sort">
-                    <option value="ascending">Ascending</option>
-                    <option value="descending ">Descending </option>
+                Sort By ↑↓:
+                <select className="poke-sort" onChange={this.handleSortOrder}>
+                    <option value="Ascending">Ascending</option>
+                    <option value="Descending">Descending</option>
                 </select>
+            </div>
+            <div className="search-bar">
+                <input type="text" onChange={this.handleSearch} placeholder="Search for your favorite Pokemon.."></input>
             </div>
             <div className='poke-gallery'>
                 <PokeList filteredPokeData={filteredPokeData} />
